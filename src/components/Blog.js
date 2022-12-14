112b4e4462481bad59496aaad5696bcd165a3f55
chheangg <balldremz@gmail.com>
2022-12-14 10:00:46 +0700

    Complete part 5.7 on adding new details to Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisibile = { display: showDetail ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        
+        <button onClick={() => setShowDetail(!showDetail)}>view</button>
+      </div>
+      <div style={showWhenVisibile}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}</div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
