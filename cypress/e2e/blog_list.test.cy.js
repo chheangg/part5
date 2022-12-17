a3763181835db9b9944b98329c87875a770bde2f
chheangg <balldremz@gmail.com>
2022-12-17 16:17:37 +0700

    Complete part 5.18 on testing login flow

--- cypress/e2e/blog_list.test.cy.js
+++ cypress/e2e/blog_list.test.cy.js
@@ -1,10 +1,35 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3000/api/testing')
+    cy.request('POST', 'http://localhost:3000/api/users/register', {
+      username: 'chheangg',
+      name: 'chheang',
+      password: 'password'
+    })
     cy.visit('http://localhost:3000/')
   })
 
   it('show login form', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credential', function() {
+      cy.get('#username').type('chheangg')
+      cy.get('#password').type('password')
+      cy.get('form').find('button').click()
+
+      cy.contains('chheang logged in')
+    })
+
+    it('fails with wrong credential', function() {
+      cy.get('#username').type('chhheangg')
+      cy.get('#password').type('password')
+      cy.get('form').find('button').click()
+
+      cy.get('.notification').should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-color', 'rgb(255, 0, 0)')
+    })
   })
 })
